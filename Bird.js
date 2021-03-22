f33421002d7456f8b89da81585988dd0eb9c10f1
rprohan06 <74810498+rprohan06@users.noreply.github.com>
2021-03-22 14:54:12 +0530

    Add files via upload

--- Bird.js
+++ Bird.js
@@ -1,7 +1,7 @@
 class Bird extends Baseclass{
     constructor(x, y) {
       super(x,y,50,50)
-      this.image=loadImage("sprites/Bird.png")
+      this.image=loadImage("sprites/bird.png")
     }
     display(){
 var pos=this.body.position
